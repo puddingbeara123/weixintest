513d20df05964a8ce2568a19863c4874d79303ca
frieda <291284353@qq.com>
2019-03-16 16:05:44 +0800

    1.0初始化布局

---
+++ src/pages/cart/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+
+  </view>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+  
+   
+  }
+
+ 
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+ <view>
+   <navigator class="searchInput" src="pages/search/main">
+    <icon type="search" size="20"/>
+   搜索
+   </navigator>
+ </view>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+  
+  },
+
+  
+}
+</script>
+
+<style >
+.searchInput{
+  height:60rpx;
+  line-height: 60rpx;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  border:1rpx solid #ff2d4a;
+}
+</style>
