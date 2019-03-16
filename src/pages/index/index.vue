0b166887f849fb27c6b72b3537bfcb645ad675a2
frieda <291284353@qq.com>
2019-03-16 19:17:27 +0800

    上存github-text

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,36 +1,54 @@
 <template>
- <view>
-   <navigator class="searchInput" src="pages/search/main">
-    <icon type="search" class="search-icon" size="20"/>
-   搜索
-   </navigator>
- </view>
+  <view>
+    <navigator class="searchInput" src="pages/search/main">
+      <view class="search-in">
+        <icon type="search" class="search-icon" size="20"/>搜索
+      </view>
+    </navigator>
+    <swiper
+  indicator-dots
+  autoplay
+  interval="2000"
+  duration="1000"
+  circular
+  indicator-color="rgba(255,255,255,0.3)"
+  ndicator-active-color="rgba(255,255,255,1)"
+
+>
+  <block>
+    <swiper-item>
+      <image src="{{item}}" class="slide-image" width="355" height="150" />
+    </swiper-item>
+  </block>
+</swiper>
+  </view>
 </template>
 
 <script>
-
-
 export default {
-  data () {
-  
-  },
-
-  
-}
+  data() {}
+};
 </script>
 
 <style >
 .searchInput{
-  height:60rpx;
+  background-color: #ff2d4a;
+  padding:20rpx;
+}
+.search-in {
+  height: 60rpx;
   line-height: 60rpx;
-  display:flex;
+  font-size:36rpx;
+  display: flex;
+  background-color: #fff;
   justify-content: center;
   align-items: center;
-  border:1rpx solid #ff2d4a;
-  border-radius:10rpx;
-  color:#666;
+  border: 1rpx solid #ff2d4a;
+  border-radius: 10rpx;
+  color: #666;
 }
-.search-icon{
-  margin-right:10rpx;
+.search-icon {
+  margin-right: 10rpx;
+  margin-top: 30rpx;
 }
 </style>
