e90a5a94059f9aeac7d4c7cf8911e09486380561
frieda <291284353@qq.com>
2019-03-18 20:30:01 +0800

    分类页面测试完成

---
+++ src/pages/my/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <view>
+
+  </view>
+   
+</template>
+
+<script>
+// 引入组件
+
+
+export default {
+ data() {
+    return {
+
+    }
+  },
+  
+  onLoad(){
+    
+
+  }
+};
+</script>
+
+<style >
+
+
+</style>
